843c9b1e651f205b045f8cdee43fa065de25f794
Rui <53028949+yehruei@users.noreply.github.com>
2024-11-19 00:25:53 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/tutorial/', // 替换为您的仓库名称
+  base: '/',
   build: {
     outDir: 'dist'
   }
